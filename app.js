8eb47c12cf496b1193bc8e7d1a9b638e93355eb3
Garrett Griffin <grgisme@gmail.com>
2016-04-17 18:27:33 -0400

    Initial Attempt

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 var alexa = require('alexa-app');
 var app = new alexa.app('tivo_control');
 
-app.intent('SendCommand',
-    {
-        "slots":{"command":"COMMAND"}
-        ,"utterances":[ "say the command {1-100|command}" ]
-    },
-    function(request,response) {
-        var command = request.slot('command');
-        response.say("You said the command "+command);
+app.launch(function(request,response) {
+    response.say("Welcome to Tivo Control");
+});
+
+app.intent('pause', function(request,response) {
+        response.say("You told me to pause.");
     }
 );
 
-// Manually hook the handler function into express 
-express.post('/tivo_control',function(req,res) {
-    app.request(req.body)        // connect express to alexa-app
-        .then(function(response) { // alexa-app returns a promise with the response
-            res.json(response);      // stream it to express' output
-        });
-});
+app.intent('play', function(request,response) {
+        response.say("You told me to play.");
+    }
+);
+
+var express_app = express();
+express_app.listen(4568);
+
+app.express(express_app, "/tivo_control", false);
